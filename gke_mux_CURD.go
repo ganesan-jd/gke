bd5e61121beaec0cca8c685f861ad394f14e12cf
sanativesystems <info.sanativesystems@gmail.com>
2020-09-08 14:08:10 +0000

    Adding Viper config module

--- gke_mux_CURD.go
+++ gke_mux_CURD.go
@@ -6,16 +6,49 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"fmt"
 
 	"github.com/gorilla/mux"
 	"golang.org/x/net/context"
 	"google.golang.org/api/container/v1"
 	"google.golang.org/api/option"
+	"github.com/spf13/viper"
 )
 
-var Zone = "us-central1-c"
-var JsonPath = "/etc/gke/credentials.json"
-var ProjectID = "kubernetes-283301"
+type Configuration struct {
+        Server Server
+        App    App
+}
+
+type Server struct {
+        Port string
+}
+
+type App struct {
+        Zone string
+        ProjectID string
+        CredentialsPath string
+}
+
+var (
+    global_config Configuration
+)
+
+func read_config_file(configFile string)(bool, error) {
+	viper.SetConfigName(configFile)
+	viper.AddConfigPath(".")
+        viper.AddConfigPath("/etc/gke")
+        viper.AutomaticEnv()
+
+        if err := viper.ReadInConfig(); err != nil {
+                return false, fmt.Errorf("Error reading config file, %s", err)
+        }
+        err := viper.Unmarshal(&global_config)
+        if err != nil {
+                return false, fmt.Errorf("Error unmarshal config file, %s", err)
+        }
+        return true, nil
+}
 
 func gke_create(w http.ResponseWriter, r *http.Request) {
 
@@ -25,13 +58,16 @@ func gke_create(w http.ResponseWriter, r *http.Request) {
 
 	ctx := context.Background()
 
-	containerService, err := container.NewService(ctx, option.WithCredentialsFile(JsonPath))
+	containerService, err := container.NewService(ctx, option.WithCredentialsFile(global_config.App.CredentialsPath))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err := containerService.Projects.Zones.Clusters.Create(ProjectID, Zone, &art).Context(ctx).Do()
+	resp, err := containerService.Projects.Zones.Clusters.Create(global_config.App.ProjectID, global_config.App.Zone, &art).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	json.NewEncoder(w).Encode(resp)
 
@@ -41,11 +77,11 @@ func gke_list(w http.ResponseWriter, r *http.Request) {
 
 	ctx := context.Background()
 
-	containerService, err := container.NewService(ctx, option.WithCredentialsFile(JsonPath))
+	containerService, err := container.NewService(ctx, option.WithCredentialsFile(global_config.App.CredentialsPath))
 	if err != nil {
 		log.Fatal(err)
 	}
-	resp, err := containerService.Projects.Zones.Clusters.List(ProjectID, Zone).Context(ctx).Do()
+	resp, err := containerService.Projects.Zones.Clusters.List(global_config.App.ProjectID, global_config.App.Zone).Context(ctx).Do()
 
 	json.NewEncoder(w).Encode(resp)
 }
@@ -54,11 +90,11 @@ func gke_get(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	clusterid := vars["clusterid"]
 	ctx := context.Background()
-	containerService, err := container.NewService(ctx, option.WithCredentialsFile(JsonPath))
+	containerService, err := container.NewService(ctx, option.WithCredentialsFile(global_config.App.CredentialsPath))
 	if err != nil {
 		log.Fatal(err)
 	}
-	resp, err := containerService.Projects.Zones.Clusters.Get(ProjectID, Zone, clusterid).Context(ctx).Do()
+	resp, err := containerService.Projects.Zones.Clusters.Get(global_config.App.ProjectID, global_config.App.Zone, clusterid).Context(ctx).Do()
 	json.NewEncoder(w).Encode(resp)
 }
 
@@ -66,16 +102,21 @@ func gke_delete(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	clusterid := vars["clusterid"]
 	ctx := context.Background()
-	containerService, err := container.NewService(ctx, option.WithCredentialsFile(JsonPath))
+	containerService, err := container.NewService(ctx, option.WithCredentialsFile(global_config.App.CredentialsPath))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err := containerService.Projects.Zones.Clusters.Delete(ProjectID, Zone, clusterid).Context(ctx).Do()
+	resp, err := containerService.Projects.Zones.Clusters.Delete(global_config.App.ProjectID, global_config.App.Zone, clusterid).Context(ctx).Do()
 	json.NewEncoder(w).Encode(resp)
 }
 
 func main() {
+	var configfilename = "app"
+	var _, err = read_config_file(configfilename)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	router := mux.NewRouter()
 
